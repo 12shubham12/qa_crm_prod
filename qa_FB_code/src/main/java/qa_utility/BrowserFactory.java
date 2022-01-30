cfb42e3a7a193d5cfae7e443b3d77c1da12fed13
Shubham Raj <shuraj93@gmail.com>
2022-01-31 00:07:26 +0530

    third commit

--- qa_FB_code/src/main/java/qa_utility/BrowserFactory.java
+++ qa_FB_code/src/main/java/qa_utility/BrowserFactory.java
@@ -1,5 +1,37 @@
 package qa_utility;
 
-public class BrowserFactory {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 
+public class BrowserFactory {
+	
+public static WebDriver startApplication(WebDriver driver, String browser, String url){		
+		
+		if(browser.equalsIgnoreCase("firefox")){
+		System.setProperty("webdriver.gecko.driver", "./drivers/geckodriver.exe");
+		driver = new FirefoxDriver();
+		}
+		else if(browser.equalsIgnoreCase("chrome")){
+			System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
+			driver = new ChromeDriver();
+			}
+		else if(browser.equalsIgnoreCase("ie")){
+			System.setProperty("webdriver.ie.driver", "./drivers/IEDriverServer.exe");
+			driver = new InternetExplorerDriver();
+			}
+		else{
+			System.out.println("No such browser");
+		}
+		
+		driver.manage().window().maximize();
+		driver.get(url);
+		return driver;
+	}
+	
+	//quit browser
+	public static void quitBrowser(WebDriver driver){
+		driver.quit();
+	}
 }
